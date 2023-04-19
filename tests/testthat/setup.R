d19e9e09fef33a0088b578a894637ffba6d96259
barrettk <barrettk@metrumrg.com>
2023-04-19 19:59:01 -0400

    finalized setup function for making fake packages  - have working tests for covr and rcmdcheck  - tests are performed at the same time to save time  - renamed test file name for above reason

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,14 +1,7 @@
 
 library(usethis)
 
-### This will have to be changes, but structuring tests around this for now ###
-package_dir <- file.path("/data", "Projects", "package_dev")
-
-
-pkg_tar <- file.path(package_dir,"review_3.1.0.tar.gz") # tarball
-out_dir <- file.path(tempdir(), "results")
-
-
+# Base rcmdcheck args. Set the `path` element to the file path of the tarball before using
 rcmdcheck_args = list(
   timeout = Inf,
   args = "--no-manual",
@@ -16,93 +9,19 @@ rcmdcheck_args = list(
 )
 
 
-setup_files <- function(pkg_tar){
-  # Unpack tarball
-  pkg_source_path <- unpack_tarball(pkg_tar)
-
-  # Get package name and version
-  pkg_desc <- get_pkg_desc(pkg_source_path, fields = c("Package", "Version"))
-  pkg_name <- pkg_desc$Package
-  pkg_ver <- pkg_desc$Version
-  pkg_name_ver <- paste0(pkg_name, "_", pkg_ver)
-
-  return(
-    list(
-      pkg_source_path = pkg_source_path,
-      pkg_name = pkg_name,
-      pkg_ver = pkg_ver,
-      pkg_name_ver = pkg_name_ver
-    )
-  )
-}
-
-
-# create_failing_package <- function(
-    #     pkg_name = "mypackage",
-#     type = c("fail_func", "fail_test")
-# ){
-#
-#   type <- match.arg(type)
-#
-#   # Create temp package
-#   temp_dir <- tempfile("pkg_temp_")
-#   pkg_dir <- file.path(temp_dir, pkg_name)
-#   fs::dir_create(pkg_dir)
-#   devtools::create(pkg_dir)
-#
-#   # Add a file with a syntax error to the package
-#   script_file <- file.path(pkg_dir, "R", "myscript.R")
-#   fs::file_create(script_file)
-#   if(type == "fail_func"){
-#     writeLines("myfunction <- function(x { x + 1", script_file)
-#   }else{
-#     writeLines("myfunction <- function(x) { x + 1}", script_file)
-#   }
-#
-#
-#   # Add a test setup for running test suite
-#   test_dir <- file.path(pkg_dir, "tests", "testthat")
-#   testthat_script <- file.path(dirname(test_dir), "testthat.R")
-#   fs::dir_create(test_dir, recurse = TRUE)
-#   fs::file_create(testthat_script)
-#   test_setup <- glue::glue("
-#     library(testthat)
-#     library({pkg_name})
-#
-#     test_check('{pkg_name}')
-#     ")
-#   writeLines(test_setup, testthat_script)
-#
-#   # Add a test file that will fail
-#   test_file <- file.path(test_dir, "test-myscript.R")
-#   fs::file_create(test_file)
-#   # Test will fail regardless
-#   writeLines("testthat::test_that('this fails', { myfunction('blah') })", test_file)
-#
-#
-#   pkg_desc_path <- file.path(pkg_dir, "DESCRIPTION")
-#
-#   # Create temp directory for saving results, that follows the convention of this package:
-#   # namley, a directory with the basename of the package and version
-#   desc_file <- read.dcf(pkg_desc_path, fields = c("Package", "Version"))[1L,]
-#   pkg_name_ver <- paste0(desc_file, collapse = "_")
-#   results_dir <- file.path(temp_dir, "results", pkg_name_ver)
-#   fs::dir_create(results_dir)
-#
-#   # Build the package tarball
-#   tar_file <- devtools::build(pkg_dir)
-#
-#   return(
-#     list(
-#       temp_dir = temp_dir,
-#       pkg_dir = pkg_dir,
-#       tar_file = tar_file,
-#       results_dir = results_dir
-#     )
-#   )
-# }
-
-
+#' Creates and builds a fake package
+#'
+#' @param pkg_name a name for the package. Useful to specify for informative messages or creating multiple packages at once
+#' @param type type of package to make. See details.
+#'
+#' @details
+#' pass_success should pass with flying colors
+#' pass_warning is meant to trigger rcmdcheck warnings
+#' fail_func vs fail_test can be used to trigger various failures - might be expanded on if needed
+#'
+#' @returns a list of file paths
+#'
+#' @keywords internal
 create_testing_package <- function(
     pkg_name = "mypackage",
     type = c("pass_success", "pass_warning", "fail_func", "fail_test")
@@ -114,7 +33,6 @@ create_testing_package <- function(
   temp_dir <- tempfile("pkg_temp_")
   pkg_dir <- file.path(temp_dir, pkg_name)
   fs::dir_create(pkg_dir)
-  # devtools::create(pkg_dir)
   create_package(pkg_dir, rstudio = FALSE, open = FALSE,
                  fields = list(
                    License = "MIT + file LICENSE",
@@ -124,10 +42,20 @@ create_testing_package <- function(
                  )
   )
 
-  # Add a file with a syntax error to the package
+  # If success - need to add license file to suppress warnings
+  if(type == "pass_success"){
+    # Add license file and suggested packages to avoid warnings
+    local_project(pkg_dir, setwd = FALSE)
+    use_mit_license(copyright_holder = NULL)
+    use_package("testthat", type = "Suggests", min_version = NULL)
+    withr::deferred_run()
+  }
+
+  # Add function to R/
   script_file <- file.path(pkg_dir, "R", "myscript.R")
   fs::file_create(script_file)
   if(type == "fail_func"){
+    # Add a file with a syntax error to the package
     writeLines("myfunction <- function(x { x + 1", script_file)
   }else{
     writeLines("myfunction <- function(x) { x + 1}", script_file)
@@ -146,10 +74,11 @@ create_testing_package <- function(
     ")
   writeLines(test_setup, testthat_script)
 
-  # Add a test file that can fail
+  # Add a test file to tests/testthat/
   test_file <- file.path(test_dir, "test-myscript.R")
   fs::file_create(test_file)
   if(type == "fail_test"){
+    # Add a test file that will fail
     test_lines <- "testthat::test_that('this fails', { myfunction('blah') })"
   }else{
     test_lines <- "testthat::test_that('this works', { expect_equal(myfunction(1), 2)})"
@@ -157,39 +86,9 @@ create_testing_package <- function(
   writeLines(test_lines, test_file)
 
 
-  pkg_desc_path <- file.path(pkg_dir, "DESCRIPTION")
-
-  # If success - need to update description file and add license
-  if(type == "pass_success"){
-    # desc_file <- read.dcf(pkg_desc_path)[1L,]
-    desc_file <- usethis::use_description_defaults(
-      package = pkg_name,
-      fields = list(
-        License = "MIT + file LICENSE",
-        `Authors@R` = 'person("Jane", "Doe", email = "jane@example.com",
-                          role = c("aut", "cre"),
-                          comment = c(ORCID = "YOUR-ORCID-ID"))'
-      )
-    )
-
-    write.dcf(desc_file, pkg_desc_path)
-
-    # Add license file to avoid warnings - the method can hopefully be improved (see notes)
-    old_proj <- proj_get()
-    withr::with_dir(pkg_dir, { # couldnt get `use_mit_license` to work this way - uses `proj_get` under the hood (currently doesnt do anything)
-      # proj_set seemed required - couldnt find a workaround, but this is definitely not ideal and may have unintended consequences
-      # (also probably wont work on drone - hunch)
-      proj_set(path = pkg_dir, force = FALSE)
-      use_mit_license(copyright_holder = NULL)
-      use_package("testthat", type = "Suggests", min_version = NULL)
-    })
-    proj_set(path = old_proj, force = FALSE)
-  }
-
-
   # Create temp directory for saving results, that follows the convention of this package:
-  # namley, a directory with the basename of the package and version
-  desc_file <- read.dcf(pkg_desc_path, fields = c("Package", "Version"))[1L,]
+  # namely, a directory with the basename of the package and version
+  desc_file <- get_pkg_desc(pkg_dir, fields = c("Package", "Version"))
   pkg_name_ver <- paste0(desc_file, collapse = "_")
   results_dir <- file.path(temp_dir, "results", pkg_name_ver)
   fs::dir_create(results_dir)
@@ -212,5 +111,34 @@ create_testing_package <- function(
 cleanup_temp_dir <- function(dir){
   unlink(dir, recursive = TRUE)
   exists <- fs::dir_exists(dir)
-  tibble::tibble(file = names(exists), exists = unname(exists))
+  tibble::tibble(file_path = names(exists), exists = unname(exists))
 }
+
+
+
+
+### This will have to be changes, but structuring tests around this for now ###
+# package_dir <- file.path("/data", "Projects", "package_dev")
+# pkg_tar <- file.path(package_dir,"review_3.1.0.tar.gz") # tarball
+# out_dir <- file.path(tempdir(), "results")
+
+# A function like this could be used if we wanted to test real packages, but unlikely (keeping during development)
+# setup_files <- function(pkg_tar){
+#   # Unpack tarball
+#   pkg_source_path <- unpack_tarball(pkg_tar)
+#
+#   # Get package name and version
+#   pkg_desc <- get_pkg_desc(pkg_source_path, fields = c("Package", "Version"))
+#   pkg_name <- pkg_desc$Package
+#   pkg_ver <- pkg_desc$Version
+#   pkg_name_ver <- paste0(pkg_name, "_", pkg_ver)
+#
+#   return(
+#     list(
+#       pkg_source_path = pkg_source_path,
+#       pkg_name = pkg_name,
+#       pkg_ver = pkg_ver,
+#       pkg_name_ver = pkg_name_ver
+#     )
+#   )
+# }
---
+++ tests/testthat/test-results.R
@@ -0,0 +1,154 @@
+
+
+describe("covr and rcmdcheck success", {
+
+  it("no rcmdcheck warnings", {
+    # Create temp package that will succeed
+    pkg_setup <- create_testing_package(type = "pass_success")
+    on.exit(cleanup_temp_dir(pkg_setup$temp_dir))
+
+    # For inspecting
+    # rstudioapi::filesPaneNavigate(pkg_setup$pkg_dir)
+
+    # Run check and coverage - expect message
+    rcmdcheck_args$path <- pkg_setup$tar_file
+    res_check <- add_rcmdcheck(pkg_setup$results_dir, rcmdcheck_args)
+    expect_message(
+      res_check <- add_rcmdcheck(pkg_setup$results_dir, rcmdcheck_args),
+      "rcmdcheck for mypackage_0.0.0.9000 passed"
+    )
+    res_covr <- add_coverage(pkg_setup$pkg_dir, pkg_setup$results_dir)
+
+
+    # confirm success
+    expect_equal(res_check, 1)
+    expect_equal(res_covr, 1)
+
+    # check rcmdcheck output
+    check_output <- readRDS(get_result_path(pkg_setup$results_dir, "check.rds"))
+    expect_equal(check_output$status, 0)
+    expect_true(rlang::is_empty(check_output$errors))
+    expect_true(rlang::is_empty(check_output$warnings))
+    expect_true(rlang::is_empty(check_output$test_fail))
+
+    # check covr output
+    covr_output <- readRDS(get_result_path(pkg_setup$results_dir, "covr.rds"))
+    expect_true(is.na(covr_output$errors))
+    expect_equal(covr_output$coverage$totalcoverage, 100)
+  })
+
+  it("with warnings", {
+    # Create temp package that will succeed with warnings
+    pkg_setup <- create_testing_package(type = "pass_warning")
+    on.exit(cleanup_temp_dir(pkg_setup$temp_dir))
+
+    # For inspecting
+    # rstudioapi::filesPaneNavigate(pkg_setup$pkg_dir)
+
+    # Run check and coverage - expect message
+    rcmdcheck_args$path <- pkg_setup$tar_file
+    expect_message(
+      res_check <- add_rcmdcheck(pkg_setup$results_dir, rcmdcheck_args),
+      "rcmdcheck for mypackage_0.0.0.9000 passed with warnings"
+    )
+    res_covr <- add_coverage(pkg_setup$pkg_dir, pkg_setup$results_dir)
+
+    # confirm success - rcmdcheck has warnings
+    expect_equal(res_check, 0.5)
+    expect_equal(res_covr, 1)
+
+    # check rcmdcheck output
+    check_output <- readRDS(get_result_path(pkg_setup$results_dir, "check.rds"))
+    expect_equal(check_output$status, 0)
+    expect_true(rlang::is_empty(check_output$errors))
+    expect_true(!rlang::is_empty(check_output$warnings))
+    expect_true(rlang::is_empty(check_output$test_fail))
+
+    # check covr output
+    covr_output <- readRDS(get_result_path(pkg_setup$results_dir, "covr.rds"))
+    expect_true(is.na(covr_output$errors))
+    expect_equal(covr_output$coverage$totalcoverage, 100)
+  })
+
+})
+
+
+
+
+describe("covr and rcmdcheck failures", {
+
+  it("failing tests", {
+    # Create temp package that will fail
+    pkg_setup <- create_testing_package(type = "fail_test")
+    on.exit(cleanup_temp_dir(pkg_setup$temp_dir))
+
+    # For inspecting
+    # rstudioapi::filesPaneNavigate(pkg_setup$pkg_dir)
+
+    # Run check and coverage - expect message
+    rcmdcheck_args$path <- pkg_setup$tar_file
+    expect_message(
+      res_check <- add_rcmdcheck(pkg_setup$results_dir, rcmdcheck_args),
+      "rcmdcheck for mypackage_0.0.0.9000 failed"
+    )
+    expect_message(
+      res_covr <- add_coverage(pkg_setup$pkg_dir, pkg_setup$results_dir),
+      "R coverage for mypackage_0.0.0.9000 failed"
+    )
+
+    # confirm failure
+    expect_equal(res_check, 0)
+    expect_true(is.na(res_covr))
+
+    # check rcmdcheck output
+    check_output <- readRDS(get_result_path(pkg_setup$results_dir, "check.rds"))
+    expect_equal(check_output$status, 1)
+    expect_true(!rlang::is_empty(check_output$errors))
+    expect_true(!rlang::is_empty(check_output$warnings))
+    expect_true(!rlang::is_empty(check_output$test_fail))
+
+    # check covr output
+    covr_output <- readRDS(get_result_path(pkg_setup$results_dir, "covr.rds"))
+    expect_true(!is.na(covr_output$errors))
+    expect_true(is.na(covr_output$coverage$totalcoverage)) # technically tested above as well
+  })
+
+
+  it("bad functions - failure before tests are run", {
+    # Create temp package that will fail
+    pkg_setup <- create_testing_package(type = "fail_func")
+    on.exit(cleanup_temp_dir(pkg_setup$temp_dir))
+
+    # For inspecting
+    # rstudioapi::filesPaneNavigate(pkg_setup$pkg_dir)
+
+    # Run check and coverage - expect message
+    rcmdcheck_args$path <- pkg_setup$tar_file
+    expect_message(
+      res_check <- add_rcmdcheck(pkg_setup$results_dir, rcmdcheck_args),
+      "rcmdcheck for mypackage_0.0.0.9000 failed"
+    )
+    expect_message(
+      res_covr <- add_coverage(pkg_setup$pkg_dir, pkg_setup$results_dir),
+      "R coverage for mypackage_0.0.0.9000 failed"
+    )
+
+    # confirm failure
+    expect_equal(res_check, 0)
+    expect_true(is.na(res_covr))
+
+    # check rcmdcheck output
+    check_output <- readRDS(get_result_path(pkg_setup$results_dir, "check.rds"))
+    expect_equal(check_output$status, 1)
+    expect_true(!rlang::is_empty(check_output$errors))
+    expect_true(rlang::is_empty(check_output$warnings))
+    expect_true(rlang::is_empty(check_output$test_fail))
+
+    # check covr output
+    covr_output <- readRDS(get_result_path(pkg_setup$results_dir, "covr.rds"))
+    expect_true(!is.na(covr_output$errors$message))
+    expect_true(is.na(covr_output$coverage$totalcoverage)) # technically tested above as well
+  })
+
+})
+
