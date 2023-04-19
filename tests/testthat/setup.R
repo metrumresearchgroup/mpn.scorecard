16ca3f0dacbd79122b6ce5ab3874fd2f7cc877a5
barrettk <barrettk@metrumrg.com>
2023-04-19 18:56:54 -0400

    setup functions for creating fake tarballs (WIP)

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,4 +1,6 @@
 
+library(usethis)
+
 ### This will have to be changes, but structuring tests around this for now ###
 package_dir <- file.path("/data", "Projects", "package_dev")
 
@@ -35,15 +37,92 @@ setup_files <- function(pkg_tar){
 }
 
 
-create_failing_package <- function(type = c("fail_func", "fail_test")){
+# create_failing_package <- function(
+    #     pkg_name = "mypackage",
+#     type = c("fail_func", "fail_test")
+# ){
+#
+#   type <- match.arg(type)
+#
+#   # Create temp package
+#   temp_dir <- tempfile("pkg_temp_")
+#   pkg_dir <- file.path(temp_dir, pkg_name)
+#   fs::dir_create(pkg_dir)
+#   devtools::create(pkg_dir)
+#
+#   # Add a file with a syntax error to the package
+#   script_file <- file.path(pkg_dir, "R", "myscript.R")
+#   fs::file_create(script_file)
+#   if(type == "fail_func"){
+#     writeLines("myfunction <- function(x { x + 1", script_file)
+#   }else{
+#     writeLines("myfunction <- function(x) { x + 1}", script_file)
+#   }
+#
+#
+#   # Add a test setup for running test suite
+#   test_dir <- file.path(pkg_dir, "tests", "testthat")
+#   testthat_script <- file.path(dirname(test_dir), "testthat.R")
+#   fs::dir_create(test_dir, recurse = TRUE)
+#   fs::file_create(testthat_script)
+#   test_setup <- glue::glue("
+#     library(testthat)
+#     library({pkg_name})
+#
+#     test_check('{pkg_name}')
+#     ")
+#   writeLines(test_setup, testthat_script)
+#
+#   # Add a test file that will fail
+#   test_file <- file.path(test_dir, "test-myscript.R")
+#   fs::file_create(test_file)
+#   # Test will fail regardless
+#   writeLines("testthat::test_that('this fails', { myfunction('blah') })", test_file)
+#
+#
+#   pkg_desc_path <- file.path(pkg_dir, "DESCRIPTION")
+#
+#   # Create temp directory for saving results, that follows the convention of this package:
+#   # namley, a directory with the basename of the package and version
+#   desc_file <- read.dcf(pkg_desc_path, fields = c("Package", "Version"))[1L,]
+#   pkg_name_ver <- paste0(desc_file, collapse = "_")
+#   results_dir <- file.path(temp_dir, "results", pkg_name_ver)
+#   fs::dir_create(results_dir)
+#
+#   # Build the package tarball
+#   tar_file <- devtools::build(pkg_dir)
+#
+#   return(
+#     list(
+#       temp_dir = temp_dir,
+#       pkg_dir = pkg_dir,
+#       tar_file = tar_file,
+#       results_dir = results_dir
+#     )
+#   )
+# }
+
+
+create_testing_package <- function(
+    pkg_name = "mypackage",
+    type = c("pass_success", "pass_warning", "fail_func", "fail_test")
+){
 
   type <- match.arg(type)
 
   # Create temp package
   temp_dir <- tempfile("pkg_temp_")
-  pkg_dir <- file.path(temp_dir, "mypackage")
+  pkg_dir <- file.path(temp_dir, pkg_name)
   fs::dir_create(pkg_dir)
-  devtools::create(pkg_dir)
+  # devtools::create(pkg_dir)
+  create_package(pkg_dir, rstudio = FALSE, open = FALSE,
+                 fields = list(
+                   License = "MIT + file LICENSE",
+                   `Authors@R` = 'person("Jane", "Doe", email = "jane@example.com",
+                          role = c("aut", "cre"),
+                          comment = c(ORCID = "YOUR-ORCID-ID"))'
+                 )
+  )
 
   # Add a file with a syntax error to the package
   script_file <- file.path(pkg_dir, "R", "myscript.R")
@@ -54,7 +133,6 @@ create_failing_package <- function(type = c("fail_func", "fail_test")){
     writeLines("myfunction <- function(x) { x + 1}", script_file)
   }
 
-
   # Add a test setup for running test suite
   test_dir <- file.path(pkg_dir, "tests", "testthat")
   testthat_script <- file.path(dirname(test_dir), "testthat.R")
@@ -62,17 +140,60 @@ create_failing_package <- function(type = c("fail_func", "fail_test")){
   fs::file_create(testthat_script)
   test_setup <- glue::glue("
     library(testthat)
-    library(mypackage)
+    library({pkg_name})
 
-    test_check('mypackage')
+    test_check('{pkg_name}')
     ")
   writeLines(test_setup, testthat_script)
 
-  # Add a test file that will fail
+  # Add a test file that can fail
   test_file <- file.path(test_dir, "test-myscript.R")
   fs::file_create(test_file)
-  # Test will fail regardless
-  writeLines("testthat::test_that('this fails', { myfunction('blah') })", test_file)
+  if(type == "fail_test"){
+    test_lines <- "testthat::test_that('this fails', { myfunction('blah') })"
+  }else{
+    test_lines <- "testthat::test_that('this works', { expect_equal(myfunction(1), 2)})"
+  }
+  writeLines(test_lines, test_file)
+
+
+  pkg_desc_path <- file.path(pkg_dir, "DESCRIPTION")
+
+  # If success - need to update description file and add license
+  if(type == "pass_success"){
+    # desc_file <- read.dcf(pkg_desc_path)[1L,]
+    desc_file <- usethis::use_description_defaults(
+      package = pkg_name,
+      fields = list(
+        License = "MIT + file LICENSE",
+        `Authors@R` = 'person("Jane", "Doe", email = "jane@example.com",
+                          role = c("aut", "cre"),
+                          comment = c(ORCID = "YOUR-ORCID-ID"))'
+      )
+    )
+
+    write.dcf(desc_file, pkg_desc_path)
+
+    # Add license file to avoid warnings - the method can hopefully be improved (see notes)
+    old_proj <- proj_get()
+    withr::with_dir(pkg_dir, { # couldnt get `use_mit_license` to work this way - uses `proj_get` under the hood (currently doesnt do anything)
+      # proj_set seemed required - couldnt find a workaround, but this is definitely not ideal and may have unintended consequences
+      # (also probably wont work on drone - hunch)
+      proj_set(path = pkg_dir, force = FALSE)
+      use_mit_license(copyright_holder = NULL)
+      use_package("testthat", type = "Suggests", min_version = NULL)
+    })
+    proj_set(path = old_proj, force = FALSE)
+  }
+
+
+  # Create temp directory for saving results, that follows the convention of this package:
+  # namley, a directory with the basename of the package and version
+  desc_file <- read.dcf(pkg_desc_path, fields = c("Package", "Version"))[1L,]
+  pkg_name_ver <- paste0(desc_file, collapse = "_")
+  results_dir <- file.path(temp_dir, "results", pkg_name_ver)
+  fs::dir_create(results_dir)
+
 
   # Build the package tarball
   tar_file <- devtools::build(pkg_dir)
@@ -81,10 +202,15 @@ create_failing_package <- function(type = c("fail_func", "fail_test")){
     list(
       temp_dir = temp_dir,
       pkg_dir = pkg_dir,
-      tar_file = tar_file
+      tar_file = tar_file,
+      results_dir = results_dir
     )
   )
 }
 
 
-
+cleanup_temp_dir <- function(dir){
+  unlink(dir, recursive = TRUE)
+  exists <- fs::dir_exists(dir)
+  tibble::tibble(file = names(exists), exists = unname(exists))
+}
--- tests/testthat/test-check.R
+++ tests/testthat/test-check.R
@@ -1,29 +1,56 @@
-testthat::test_that('rcmdcheck works correctly on success', {
 
-  pkg_setup <- setup_files(pkg_tar)
-  on.exit(unlink(dirname(pkg_setup$pkg_source_path), recursive = TRUE), add = TRUE)
+testthat::test_that('rcmdcheck works correctly on success - rcmdcheck success', {
 
-  out_dir_pkg <- file.path(out_dir, pkg_setup$pkg_name_ver)
+  # Create temp package that will fail
+  pkg_setup <- create_testing_package(type = "pass_success")
+  # on.exit(cleanup_temp_dir(pkg_setup$temp_dir))
+  rstudioapi::filesPaneNavigate(pkg_setup$pkg_dir)
 
   # Set path and run check
-  rcmdcheck_args$path <- pkg_tar
-  res_check <- add_rcmdcheck(out_dir_pkg, rcmdcheck_args)
+  rcmdcheck_args$path <- pkg_setup$tar_file
+  res_check <- add_rcmdcheck(pkg_setup$results_dir, rcmdcheck_args)
+
 
   # confirm success
   expect_equal(res_check, 1)
 
   # check outputs
-  check_output <- readRDS(get_result_path(out_dir_pkg, "check.rds"))
+  check_output <- readRDS(get_result_path(pkg_setup$results_dir, "check.rds"))
+  expect_equal(check_output$status, 0)
+  expect_true(rlang::is_empty(check_output$errors))
+  expect_true(rlang::is_empty(check_output$warnings))
+  expect_true(rlang::is_empty(check_output$test_fail))
+
+  cleanup_temp_dir(dirname(pkg_setup$temp_dir))
+})
+
+
+testthat::test_that('rcmdcheck works correctly on success - rcmdcheck warning', {
+
+  # Create temp package that will fail
+  pkg_setup <- create_testing_package(type = "pass_warning")
+  on.exit(unlink(pkg_setup$temp_dir))
+
+  # Set path and run check
+  rcmdcheck_args$path <- pkg_setup$tar_file
+  res_check <- add_rcmdcheck(pkg_setup$temp_dir, rcmdcheck_args)
+
+  # confirm success
+  expect_equal(res_check, 0.5)
+
+  # check outputs
+  check_output <- readRDS(get_result_path(pkg_setup$temp_dir, "check.rds"))
   expect_equal(check_output$status, 0)
   expect_true(rlang::is_empty(check_output$errors))
   expect_true(rlang::is_empty(check_output$warnings))
   expect_true(rlang::is_empty(check_output$test_fail))
 })
 
+
 testthat::test_that('rcmdcheck works correctly on failure', {
 
   # Create temp package that will fail
-  pkg_setup <- create_failing_package()
+  pkg_setup <- create_testing_package(type = "fail_test")
   on.exit(unlink(pkg_setup$temp_dir))
 
   # Set path and run check
