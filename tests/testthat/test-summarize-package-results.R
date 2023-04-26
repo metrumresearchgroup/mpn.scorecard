70b17614d2f384782880ab8a0f8778c149df3917
barrettk <barrettk@metrumrg.com>
2023-04-26 11:12:17 -0400

    fix summarize-results tests

--- tests/testthat/test-summarize-package-results.R
+++ tests/testthat/test-summarize-package-results.R
@@ -7,10 +7,10 @@ describe("summarize_pacakge_results", {
 
     pkg_results <- summarize_package_results(result_dirs)
 
-    expect_equal(dim(pkg_results), c(5, 11))
+    expect_equal(dim(pkg_results), c(7, 11))
 
     # Make sure package names are parsed correctly - integration test
-    pkg_names_expect <- paste0("package", seq(1:5))
+    pkg_names_expect <- paste0("package", seq(1:7))
     expect_equal(pkg_results$package, pkg_names_expect)
 
     # TODO: revisit this test - most of this can/should be tested via other functions called by this one
