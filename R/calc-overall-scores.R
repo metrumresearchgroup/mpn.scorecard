d988ce6fe36fb1f182f8885cbae9e7a5f4cd1f05
barrettk <barrettk@metrumrg.com>
2023-04-28 13:47:00 -0400

    added category weightings for overall score

--- R/calc-overall-scores.R
+++ R/calc-overall-scores.R
@@ -18,7 +18,12 @@ calc_overall_scores <- function(scorelist) {
    round(mean(unlist(scorelist$scores[[.x]]), na.rm = TRUE), 3)
   }) %>% purrr::set_names(categories)
 
-  scorelist$category_scores$overall <- round(mean(unlist(scorelist$category_scores), na.rm = TRUE), 3)
+  # Category weighting
+  category_score_weighting <- c(testing = 0.4, documentation = 0.2, maintenance = 0.2, transparency = 0.2)
+  # Ensure names are in the correct order
+  weighted_category_scores <- category_score_weighting * unlist(scorelist$category_scores[names(category_score_weighting)])
+
+  scorelist$category_scores$overall <- round(sum(weighted_category_scores), 3)
   ##### end stand-in code
 
   return(scorelist)
