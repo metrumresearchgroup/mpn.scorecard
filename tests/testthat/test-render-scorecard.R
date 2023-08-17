7e920c86e51f02329d7847f3b3d9c54982370aa9
Kyle Barrett <barrettk@metrumrg.com>
2023-08-17 14:02:02 -0400

    test fix: expect traceability matrix by default
     -  make sure to remove extra RDS file after running to avoid conflicts with score_pkg tests

--- tests/testthat/test-render-scorecard.R
+++ tests/testthat/test-render-scorecard.R
@@ -8,10 +8,11 @@ describe("render scorecard and scorecard summary reports", {
     # `result_dirs_select` defined in tests/testthat/setup.R
 
     pdf_paths <- purrr::map_chr(result_dirs_select, ~{
-      render_scorecard(results_dir = .x, overwrite = TRUE)
+      render_scorecard(results_dir = .x, overwrite = TRUE) %>% suppressWarnings()
     })
 
     on.exit(fs::file_delete(pdf_paths), add = TRUE)
+    on.exit(fs::file_delete(get_result_path(result_dirs_select, "export_doc.rds")), add = TRUE)
 
     # Checking multiple PDFs to account for package failures and differences in outputs
     # This will mostly be valuable if we inspect more specific elements using `pdftools::pdf_text`
@@ -19,10 +20,10 @@ describe("render scorecard and scorecard summary reports", {
 
       # Check attributes
       rendered_pdf_toc <- pdftools::pdf_toc(pdf = pdf_path.i)$children
-      expect_equal(length(rendered_pdf_toc), 3)
+      expect_equal(length(rendered_pdf_toc), 4)
 
       title_sections <- purrr::map_chr(rendered_pdf_toc, ~{.x$title})
-      expect_equal(title_sections, c("Overview", "Details", "Appendix"))
+      expect_equal(title_sections, c("Overview", "Details", "Traceability Matrix","Appendix"))
 
       title_sub_sections <- purrr::map_chr(rendered_pdf_toc[[2]]$children, ~{.x$title})
       expect_equal(length(title_sub_sections), 2)
